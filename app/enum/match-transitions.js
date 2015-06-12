9f546a6540a46616e8481f1d4d0d629c2158bce6
axelboc <axelboc@gmail.com>
2015-06-13 09:40:51 +1000

    match transitions enum

---
+++ app/enum/match-transitions.js
@@ -0,0 +1,15 @@
+
+/**
+ * Match transitions enum.
+ * @type {Object}
+ */
+module.exports = {
+	BEGIN: 'begin',
+	START_STATE: 'startestate',
+	END_STATE: 'endstate',
+	START_END_INJURY: 'startendinjury',
+	NEXT_ROUND: 'nextround',
+	BREAK: 'break',
+	RESULTS: 'results',
+	END: 'end'
+};
