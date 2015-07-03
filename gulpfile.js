8553d6c71abf3d52d62b19c1154a7e0bcbd027c1
axelboc <axelboc@gmail.com>
2015-07-04 01:30:09 +1000

    gulpfile and dependencies

---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+'use strict';
+
+// Dependencies
+var gulp = require('gulp');
+var browserify = require('browserify');
+var cache = require('gulp-cached');
+var jshint = require('gulp-jshint');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var gutil = require('gulp-util');
+var buffer = require('vinyl-buffer');
+var source = require('vinyl-source-stream');
+var path = require('path');
+
+// Path configurations
+var p = {
+	jsGlob: '**/*.js',
+	cssGlob: '**/*.css',
+	app: 'app',
+	clients: {
+		dir: 'clients',
+		jp: 'jury-president',
+		cj: 'corner-judge',
+		shared: 'shared',
+		root: 'root.js',
+	},
+	clientsDest: 'public/js',
+	styles: 'styles'
+};
+
+/**
+ * Build a client script.
+ */
+function buildClientScript(folder) {
+	return browserify({
+			entries: path.join(p.clients.dir, folder, p.clients.root),
+			debug: true
+		}).bundle()
+		.pipe(source(folder + '.js'))
+		.pipe(buffer())
+		.pipe(sourcemaps.init({ loadMaps: true }))
+			.pipe(uglify())
+			.on('error', gutil.log)
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(p.clientsDest));
+}
+
+/**
+ * Build Jury President client script with Browserify.
+ */
+gulp.task('scripts:jp', function () {
+	return buildClientScript('jury-president');
+});
+
+/**
+ * Build Corner Judge client script with Browserify.
+ */
+gulp.task('scripts:cj', function () {
+	return buildClientScript('corner-judge');
+});
+
+/**
+ * Lint all scripts.
+ * Use gulp-cached to re-lint only files that have changed.
+ */
+gulp.task('scripts:lint', function() {
+	return gulp.src(p.jsGlob)
+		.pipe(cache('scripts:lint'))
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
+
+/**
+ * Re-run tasks when files change.
+ */
+gulp.task('watch', function () {
+	//gulp.watch('templates/*.tmpl.html', ['build']);
+});
+
+/**
+ * Default task.
+ * Run with `gulp`.
+ */
+gulp.task('default', ['scripts:jp', 'scripts:cj', 'scripts:lint', 'watch']);
