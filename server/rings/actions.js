ad445990d3564f9634297e3c6ef8b527d74123cb
Axel Bocciarelli <axelboc@gmail.com>
2016-03-12 17:34:26 +1100

    add, open, close ring actions and reducer cases

--- server/rings/actions.js
+++ server/rings/actions.js
@@ -1,6 +1,23 @@
-export const INIT = 'INIT';
+export const INIT = 'rings/INIT';
+export const ADD = 'rings/ADD';
+export const OPEN = 'rings/OPEN';
+export const CLOSE = 'rings/CLOSE';
 
 export const init = (count) => ({
   type: INIT,
-  count
+  payload: { count }
+});
+
+export const add = () => ({
+  type: ADD
+});
+
+export const open = (index, jpId) => ({
+  type: OPEN,
+  payload: { index, jpId }
+});
+
+export const close = (index) => ({
+  type: CLOSE,
+  payload: { index }
 });
--- server/rings/reducer.js
+++ server/rings/reducer.js
@@ -1,18 +1,31 @@
 import { List, Map } from 'immutable';
-import { INIT } from './actions';
+import { INIT, ADD, OPEN, CLOSE } from './actions';
 
 const initialState = new List();
+
+function initRing(index) {
+  return Map({
+    index,
+    jp: null,
+    cjs: List(),
+    slotCount: 4,
+    mathes: List()
+  });
+}
 
 export default function ringsReducer(state = initialState, action) {
   switch (action.type) {
     case INIT:
-      return List(Array.from({ length: action.count }, (val, index) => Map({
-        index,
-        jp: null,
-        cjs: List(),
-        slotCount: 4,
-        mathes: List()
-      })));
+      return List(Array.from({ length: action.payload.count }, (val, index) => initRing(index)));
+    
+    case ADD:
+      return state.push(initRing(state.size));
+    
+    case OPEN:
+      return state.setIn([action.payload.index, 'jp'], action.payload.jpId);
+    
+    case CLOSE:
+      return state.setIn([action.payload.index, 'jp'], null);
     
     default:
       return state;
