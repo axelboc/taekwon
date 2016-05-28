8352a8ec2ab5b93b54b5b09a1b057330c5e595e7
Axel Bocciarelli <axelboc@gmail.com>
2016-05-28 17:02:42 +1000

    test admin presenter

--- server/__tests__/presenters/admin-presenter-test.js
+++ server/__tests__/presenters/admin-presenter-test.js
@@ -1,2 +1,15 @@
+import { Map } from 'immutable';
+import { expect } from 'chai';
 
+import adminPresenter from '../../presenters/admin-presenter';
 
+describe ('Presenter::admin', () => {
+  
+  it('presents the entire server state', () => {
+    const state = Map();
+    const presentedState = adminPresenter(state);
+    
+    expect(presentedState).to.equal(state);
+  });
+  
+});
