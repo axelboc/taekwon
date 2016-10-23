688e7128e91803cb40bfe41cffbf4decdfda0054
Axel Bocciarelli <axelboc@gmail.com>
2016-10-23 18:33:26 +1100

    vibrate when toggling backdrop visibility

--- clients/shared/backdrop.js
+++ clients/shared/backdrop.js
@@ -4,15 +4,29 @@ function Backdrop() {
 	this.root = document.getElementById('backdrop');
 	this.text = this.root.querySelector('.bdp-text');
 	this.subtext = this.root.querySelector('.bdp-subtext');
+	this.isVisible = false;
 }
 
 Backdrop.prototype.update = function (text, subtext, visible) {
 	this.text.textContent = text;
 	this.subtext.textContent = subtext;
+
+	// Vibrate
+	if (window.navigator.vibrate && visible !== this.isVisible) {
+		window.navigator.vibrate(300);
+	}
+
+	this.isVisible = visible;
 	this.root.classList.toggle('hidden', !visible);
 };
 
 Backdrop.prototype.hide = function () {
+	// Vibrate
+	if (window.navigator.vibrate && this.isVisible) {
+		window.navigator.vibrate(300);
+	}
+
+	this.isVisible = false;
 	this.root.classList.add('hidden');
 };
 
